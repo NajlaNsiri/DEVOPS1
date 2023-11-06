1145ac6446a164c92dd60beffd90bd636cbee14f
fatmatrb <fatma.trabelsi@esprit>
2023-11-06 05:02:47 +0100

    jacoco

--- DevOps_Project/src/main/java/tn/esprit/devops_project/dto/SupplierDTO.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/dto/SupplierDTO.java
@@ -5,7 +5,7 @@ import tn.esprit.devops_project.entities.SupplierCategory;
 import java.util.Set;
 
 @Data
-public class SupplierDTO {
+public class  SupplierDTO {
     private String code;
     private String label;
     private SupplierCategory supplierCategory;
