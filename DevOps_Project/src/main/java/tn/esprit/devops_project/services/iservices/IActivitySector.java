9c596493da5540cb493358aa306f4df87e9348b3
fatmatrb <fatma.trabelsi@esprit>
2023-11-06 04:15:53 +0100

    iservice

---
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/services/iservices/IActivitySector.java
@@ -0,0 +1,16 @@
+package tn.esprit.devops_project.services.iservices;
+
+import tn.esprit.devops_project.entities.ActivitySector;
+import java.util.List;
+
+public interface IActivitySector {
+    List<ActivitySector> retrieveAllActivitySectors();
+
+    ActivitySector addActivitySector(ActivitySector activitySector);
+
+    void deleteActivitySector(Long id);
+
+    ActivitySector updateActivitySector(ActivitySector activitySector);
+
+    ActivitySector retrieveActivitySector(Long id);
+}
