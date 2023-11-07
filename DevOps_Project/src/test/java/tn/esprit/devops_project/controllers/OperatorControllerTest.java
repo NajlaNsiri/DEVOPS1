5b165ffaba10a4da6021a21b7c1c0cba8353a5a7
fatmatrb <fatma.trabelsi@esprit>
2023-11-07 04:47:09 +0100

    uncovered code operator

--- DevOps_Project/src/test/java/tn/esprit/devops_project/controllers/OperatorControllerTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/controllers/OperatorControllerTest.java
@@ -5,7 +5,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import tn.esprit.devops_project.dto.OperatorDTO;
 import tn.esprit.devops_project.entities.Operator;
-import tn.esprit.devops_project.services.OperatorServiceImpl;
+import tn.esprit.devops_project.services.iservices.IOperatorService;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +16,7 @@ class OperatorControllerTest {
     private OperatorController operatorController;
 
     @Mock
-    private OperatorServiceImpl operatorService;
+    private IOperatorService operatorService;
 
     @Test
     void testRemoveOperator() {
