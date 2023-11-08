5e50dd414c65da19bd23cff46604720817df37ff
fatmatrb <fatma.trabelsi@esprit>
2023-11-08 05:04:40 +0100

    correct database test

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -1,8 +1,10 @@
 package tn.esprit.devops_project.services;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.devops_project.entities.Invoice;
 import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.entities.Supplier;
@@ -17,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class InvoiceServiceImplTest {
     @Mock
     private InvoiceRepository invoiceRepository;
