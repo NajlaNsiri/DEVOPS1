3315c28385d1d5a70bc8f65078745103627eddab
fatmatrb <fatma.trabelsi@esprit>
2023-11-08 17:05:54 +0100

    compose

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -1,9 +1,12 @@
 /*package tn.esprit.devops_project.services;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.devops_project.entities.Invoice;
 import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.entities.Supplier;
@@ -18,7 +21,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+
 @ActiveProfiles("test")
+@TestPropertySource(locations="classpath:application-test.properties")
+@RunWith(SpringRunner.class)
 class InvoiceServiceImplTest {
     @Mock
     private InvoiceRepository invoiceRepository;
